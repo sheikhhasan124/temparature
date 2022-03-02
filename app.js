7225bc4efdc6d0c17cda6ce720faf79c5a2e8872
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-02 20:07:40 +0600

    dynamic icon add

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ let API_KEY = `b7886c754d5e4912e9c7413b226f6c90`;
 
 const searchTemperature = ()=>{
   let city =  document.getElementById('city-name').value;
-    console.log(city)
+    // console.log(city)
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-    console.log(url)
+    // console.log(url)
     fetch(url)
     .then(res=> res.json())
     .then(data=> displayApi(data))
@@ -16,5 +16,10 @@ const setInnertext = (id,text)=>{
 const displayApi = (temperature)=>{
 console.log(temperature)
 setInnertext('city',temperature.name)
+setInnertext('temperature',temperature.main.temp)
+setInnertext('condition',temperature.weather[0].main)
+const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+const imagIcon = document.getElementById('weather-icon');
+imagIcon.setAttribute('src',url)
 }
 
